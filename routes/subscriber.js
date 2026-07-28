a9ebba9addbe16b525208749489f9c9d97e67542
Haimanot Beka <haimanotbeka@gmail.com>
2026-07-28 06:02:31 +0300

    subscriber module authetication error fixed

--- routes/subscriber.js
+++ routes/subscriber.js
@@ -5,9 +5,11 @@ const {
   getAllSubscribers,
 } = require("../controllers/subscriberController");
 const adminAuth = require("../middleware/adminMiddleware");
+const auth=require("../middleware/authMiddleware");
+
 
 router.post("/", subscribe);
 router.get("/unsubscribe", unsubscribe);
-router.get("/", adminAuth, getAllSubscribers);
+router.get("/",auth, adminAuth, getAllSubscribers);
 
 module.exports = router;
